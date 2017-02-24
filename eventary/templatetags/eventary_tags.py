0a8a5ed276e8ee8c20f71b708608e3ad996205c8
Pablo Vergés <pablo.verges@4teamwork.ch>
2017-02-24 10:13:02 +0100

    Adds the eventary_auth_group template tag
    - also reorganizes the template tags alphabetically
    - refactors the logic obtaining the users eventary auth group
    - fixes if if else error -> if elif else

--- eventary/templatetags/eventary_tags.py
+++ eventary/templatetags/eventary_tags.py
@@ -1,10 +1,65 @@
 from django import template
-from django.contrib.auth.models import Group, User
+from django.contrib.auth.models import User
 from django.template.loader import render_to_string
 
 from ..models import Calendar, Event
 
 register = template.Library()
+
+
+def __get_user_group(user):
+
+    if not isinstance(user, User):
+        TypeError('value needs to be of type django.contrib.auth.models.User')
+
+    groups = user.groups.filter(name__contains='eventary_')
+    number_of_groups = groups.count()
+
+    if number_of_groups == 1:
+        # get the group the user is in
+        group = groups.first().name.split('eventary_')[1]
+    elif number_of_groups > 1:
+        raise RuntimeError("User belongs to several eventary groups!")
+    else:
+        group = 'anonymous'
+
+    return group
+
+
+@register.filter(name='actions')
+def actions(value, user=None):
+
+    _group = __get_user_group(user)
+
+    if isinstance(value, Calendar):
+        return render_to_string(
+            'eventary/actions/calendars_{group_name}.html'.format(
+                group_name=_group
+            ),
+            context={'calendar': value}
+        )
+
+    if isinstance(value, Event):
+        if value.published:
+            return render_to_string(
+                'eventary/actions/events_{group_name}.html'.format(
+                    group_name=_group
+                ),
+                context={'event': value}
+            )
+        else:
+            return render_to_string(
+                'eventary/actions/proposals_{group_name}.html'.format(
+                    group_name=_group
+                ),
+                context={'event': value}
+            )
+
+
+@register.filter(name='eventary_auth_group')
+def eventary_auth_group(user):
+    group = __get_user_group(user)
+    return ' '.join(group.split('_'))
 
 
 @register.filter(name='to_full_date')
@@ -46,14 +101,6 @@ def full_date(value):
     return len(results) and ", ".join(results) or ""
 
 
-@register.simple_tag
-def url_replace(request, field, value):
-    """Replaces a specific GET parameter"""
-    _dict = request.GET.copy()
-    _dict[field] = value
-    return _dict.urlencode()
-
-
 @register.filter(name='join')
 def join(value, arg):
     return str(arg).join([
@@ -63,63 +110,16 @@ def join(value, arg):
 
 @register.filter(name='navigation')
 def navigation(user):
-
-    if not isinstance(user, User):
-        TypeError("value needs to be of type django.contrib.auth.models.User")
-
-    groups = user.groups.filter(name__contains='eventary_')
-    number_of_groups = groups.count()
-
-    if number_of_groups == 1:
-        # get the group the user is in
-        group = groups.first().name.split('eventary_')[1]
-    if number_of_groups > 1:
-        raise RuntimeError("User belongs to several eventary groups!")
-    else:
-        group = 'anonymous'
-
-    return render_to_string('eventary/navigation/{group_name}.html'.format(
-        group_name=group
-    ))
+    _group = __get_user_group(user)
+    return render_to_string(
+        'eventary/navigation/{group_name}.html'.format(group_name=_group),
+        context={'user': user},
+    )
 
 
-@register.filter(name='actions')
-def actions(value, user=None):
-
-    if user is not None and not isinstance(user, User):
-        TypeError("value needs to be of type django.contrib.auth.models.User")
-
-    group = 'anonymous'
-    if user is not None:
-        groups = user.groups.filter(name__contains='eventary_')
-        number_of_groups = groups.count()
-
-        if number_of_groups == 1:
-            # get the group the user is in
-            group = groups.first().name.split('eventary_')[1]
-        if number_of_groups > 1:
-            raise RuntimeError("User belongs to several eventary groups!")
-
-    if isinstance(value, Calendar):
-        return render_to_string(
-            'eventary/actions/calendars_{group_name}.html'.format(
-                group_name=group
-            ),
-            context={'calendar': value}
-        )
-
-    if isinstance(value, Event):
-        if value.published:
-            return render_to_string(
-                'eventary/actions/events_{group_name}.html'.format(
-                    group_name=group
-                ),
-                context={'event': value}
-            )
-        else:
-            return render_to_string(
-                'eventary/actions/proposals_{group_name}.html'.format(
-                    group_name=group
-                ),
-                context={'event': value}
-            )
+@register.simple_tag
+def url_replace(request, field, value):
+    """Replaces a specific GET parameter"""
+    _dict = request.GET.copy()
+    _dict[field] = value
+    return _dict.urlencode()
