62e76a4a4f4e97a59fa85a27ec7a89aabe8676b5
Pablo Vergés <pablo.verges@4teamwork.ch>
2017-05-04 11:24:16 +0200

    changes event image thumbnail generator
    also adds second image generator for the event's detail view

--- eventary/specs.py
+++ eventary/specs.py
@@ -3,9 +3,18 @@ from imagekit.processors import ResizeToFill
 
 
 class Thumbnail(ImageSpec):
-    processors = [ResizeToFill(160, 160)]
+    processors = [ResizeToFill(120, 120)]
     format = 'JPEG'
     options = {'quality': 90}
 
 
 register.generator('eventary:thumbnail', Thumbnail)
+
+
+class EventPicture(ImageSpec):
+    processors = [ResizeToFill(1024, 350)]
+    format = 'JPEG'
+    options = {'quality': 90}
+
+
+register.generator('eventary:eventpicture', EventPicture)
